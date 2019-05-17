3b3d6abe54f483ed7de4dac408bb91ec217a5703
LinApps <46053126+LinApps@users.noreply.github.com>
2019-05-17 23:27:11 +0430

    Add files via upload

---
+++ font.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+clear
+
+echo -e "   "
+
+echo -e "      Telegram Persian Font    "
+
+
+sleep 3
+
+echo -e "  "
+
+echo -e "     ***    LinApps   *** "
+
+
+sleep 4
+
+clear
+
+echo -e "   "
+
+echo -e  "      Please Wait ... "
+
+sleep 2
+
+clear
+
+
+cp -r .fonts .fonts.conf ~
+
+echo -e "   "
+
+echo -e "      ok :)    "
+
+
+sleep 2
+
+clear
+
